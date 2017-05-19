e968b4e3353ea30b06945e1158c20c2538515560
Carlos Guzman <cguzman.bioinformatics@gmail.com>
2017-05-19 10:29:09 -0500

    Added in DESeq2 analysis for DGE

--- bin/dge.R
+++ bin/dge.R
@@ -10,6 +10,7 @@ args <- args[-1]
 # Load libraries
 library(RUVSeq)
 library(edgeR)
+library(DESeq2)
 library(data.table)
 library(RColorBrewer)
 
@@ -25,7 +26,7 @@ merge.all <- function(x, y) {
 data <- data.frame(Reduce(merge.all, temp))
 
 # Clean sample name headers
-colnames(data) <- gsub(".mapped.bam", "", colnames(data))
+colnames(data) <- gsub(".sorted.mapped.bam", "", colnames(data))
 
 # Set GeneID as rowname
 rownames(data) <- data[,1]
@@ -68,7 +69,7 @@ pdf('PCA_plot.pdf')
 pcaplot <- plotPCA(set2, col=colors[x], cex=1.2)
 dev.off()
 
-# DGE
+# DGE edgeR
 design <- model.matrix(~x + W_1, data=pData(set2))
 y <- DGEList(counts=counts(set2), group=x)
 y <- calcNormFactors(y, method = "upperquartile")
@@ -78,7 +79,19 @@ y <- estimateGLMTagwiseDisp(y, design)
 fit <- glmFit(y, design)
 lrt <- glmLRT(fit, coef=2)
 
-# Select top genes dge with a p-value of 0.05
-top.df <- topTags(lrt, n=Inf, sort.by="logFC", p.value = 0.05)
+# Select all top genes dge with a p-value of 1
+top.df <- topTags(lrt, n=Inf, sort.by="logFC", p.value = 1)
 
+# DGE DESeq2
+dds <- DESeqDataSetFromMatrix(countData = counts(set2), colData = pData(set2), design = ~ W_1 + x)
+dds <- DESeq(dds)
+res <- results(dds)
+res_df <- as.data.frame(results)
+
+# Plot MA figure for DESeq2
+pdf('DESeq2_MAplot.pdf')
+plotMA(dds, pvalCutoff=0.05)
+dev.off()
+
+write.table(res_df, "DESeq2.dgeList.txt", row.names = T, quote = F, sep = '\t')
 write.table(top.df, "edgeR.dgeList.txt", row.names = T, quote = F, sep = '\t')
